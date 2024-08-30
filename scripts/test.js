0bd06dc1ff64a3df6d6cd907145cff15e2b29f87
slothPete7773 <Veerakit413@gmail.com>
2024-08-31 02:46:14 +0700

    change load test path param

--- scripts/test.js
+++ scripts/test.js
@@ -1,11 +1,11 @@
 import http from "k6/http";
 
 export let options = {
-    vus: 3,
+    vus: 5,
     duration: "5s",
 };
 
 export default function () {
     // http.get("http://test.k6.io");
-    http.get("http://host.docker.internal:8000");
+    http.get("http://host.docker.internal:8000/products");
 }
